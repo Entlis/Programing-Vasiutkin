76fc2f60b689cdf33742d4263fef001f08a878ab
Entlis <72922516+Entlis@users.noreply.github.com>
2020-11-12 14:34:55 +0200

    Add files via upload

---
+++ lab03/Programm 1/main.c
@@ -0,0 +1,6 @@
+#define NUM 456
+int main () {
+	int result;
+ 	result = NUM % 100 / 10;
+	return 0;
+}
---
+++ lab03/Programm 2/src/main.c
@@ -0,0 +1,10 @@
+#define GRN 100
+#define KURS_RUB 0.4
+#define KURS_DOL 28
+#define KURS_EURO 32
+int main () {
+	double rub = GRN/KURS_RUB;
+	double dol = GRN/KURS_DOL;
+	double euro = GRN/KURS_EURO;
+	return 0;
+}	
