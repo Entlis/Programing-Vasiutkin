45eafafa699fad8d51078208e049f009ba6c9f58
Entlis <72922516+Entlis@users.noreply.github.com>
2020-11-17 00:12:42 +0300

    Delete main.c

--- lab04/Programm 3/main.c
+++
@@ -1,8 +0,0 @@
-int main () {
-	float num = 123.656;
-	int num1 = num;
-	float num2 = ((num - num1) * 1000);
-	float diff = (float)num1 / (float)num2;
-	float round_diff = ((float)(((int)(diff *100)) / 100.0));
-return 0;
-}
